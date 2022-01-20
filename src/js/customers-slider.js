2a7f5537b06c69e428dce09dea49f3067477fa74
Mark Cherniatynskyi <markushalot34@gmail.com>
2022-01-20 12:40:27 +0200

    fix mobile menu

--- src/js/customers-slider.js
+++ src/js/customers-slider.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('.customer-section__slider').slick({
         arrows: false,
         dots: true,
+        autoplay: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true
+
     });
 });
