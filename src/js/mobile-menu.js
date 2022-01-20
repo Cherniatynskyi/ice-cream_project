694e1e5da7d93c16b6df438c6c8db04bacc2b812
Mark Cherniatynskyi <markushalot34@gmail.com>
2022-01-20 13:30:24 +0200

    Added menu close script, changed lang in index.html

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,11 +2,13 @@
     const refs = {
       openModalBtn: document.querySelector('.btn-open'),
       closeModalBtn: document.querySelector('.close-menu-btn'),
+      closeMobileBtn: document.querySelector('.close-mob-list'),
       modal: document.querySelector('.mob-menu'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeMobileBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
